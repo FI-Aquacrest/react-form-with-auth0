8908ad8cb069eb31f95b6325713fcece9e77069c
Sami Pitkänen <57670730+FI-Aquacrest@users.noreply.github.com>
2023-04-07 17:12:38 +0300

    Origin url fix

--- src/index.tsx
+++ src/index.tsx
@@ -19,13 +19,18 @@ const root = ReactDOM.createRoot(
 
 const theme = createTheme(lightThemeOptions);
 
+const origin =
+  window.location.hostname === 'fi-aquacrest.github.io'
+    ? 'https://fi-aquacrest.github.io/react-form-with-auth0/'
+    : window.location.origin;
+
 root.render(
   <React.StrictMode>
     <Auth0Provider
       domain="dev-0yuvhzezjxqq2qjx.us.auth0.com"
       clientId="w9g9wV7kCKhFyJcZptWGxyQHPFbKuyfs"
       authorizationParams={{
-        redirect_uri: window.location.origin,
+        redirect_uri: origin,
       }}
     >
       <ReduxProvider store={setupStore()}>
